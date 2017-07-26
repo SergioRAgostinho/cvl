4928506090d399f2ff61793849e5f1328f7faac3
Sérgio Agostinho <sergio.r.agostinho@gmail.com>
2017-07-25 17:53:31 -0700

    Added cmake dependency checker for variables

---
+++ io/include/cvl/io/mat_io.h
@@ -0,0 +1,21 @@
+/**
+  * \author Sergio Agostinho - sergio.r.agostinho@gmail.com
+  * \date created: 2017/07/25
+  * \date last modified: 2017/07/25
+  */
+#pragma once
+#ifndef CVL_IO_MAT_IO_H_
+#define CVL_IO_MAT_IO_H_
+
+namespace ht
+{
+  class MatReader
+  {
+  };
+
+  class MatWriter
+  {
+  };
+}
+
+#endif //CVL_IO_MAT_IO_H_
